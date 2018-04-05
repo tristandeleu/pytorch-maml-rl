38b720f3641b0dd57c4658b5bbab17faf258446d
arianhosseini <arian.ah@hotmail.com>
2018-04-05 14:53:13 -0400

    is cuda added to meta_learner

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import envs
 import gym
 
 env = gym.make('TabularMDP-v0')
-print env.observation_space.shape
 meta_learner = MetaLearner(env)
 print ("[*] meta learner created")
 meta_learner.meta_train()
