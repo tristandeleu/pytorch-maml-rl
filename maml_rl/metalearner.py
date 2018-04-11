a33abff5facc8b74542a8fcdc0c539eb3fcad656
Tristan Deleu <tristan.deleu@gmail.com>
2018-04-11 17:48:48 -0400

    Merge surrogate_loss and kl

--- maml_rl/metalearner.py
+++ maml_rl/metalearner.py
@@ -29,6 +29,16 @@ class MetaLearner(object):
 
         return loss
 
+    def adapt(self, episodes):
+        # Fit the baseline to the training episodes
+        self.baseline.fit(episodes)
+        # Get the loss on the training episodes
+        loss = self.inner_loss(episodes)
+        # Get the new parameters after a one-step gradient update
+        params = self.policy.update_params(loss, step_size=self.fast_lr)
+
+        return params
+
     def sample(self, meta_batch_size=20):
         tasks = self.sampler.sample_tasks(num_tasks=meta_batch_size)
         episodes = []
@@ -36,10 +46,8 @@ class MetaLearner(object):
             self.sampler.reset_task(task)
             train_episodes = self.sampler.sample(self.policy,
                 gamma=self.gamma, is_cuda=self.is_cuda)
-            self.baseline.fit(train_episodes)
-            loss = self.inner_loss(train_episodes)
 
-            params = self.policy.update_params(loss, step_size=self.fast_lr)
+            params = self.adapt(train_episodes)
 
             valid_episodes = self.sampler.sample(self.policy, params=params,
                 gamma=self.gamma, is_cuda=self.is_cuda)
@@ -47,14 +55,10 @@ class MetaLearner(object):
         return episodes
 
     def loss(self, episodes):
+        # TODO: To be deprecated
         losses = []
         for train_episodes, valid_episodes in episodes:
-            self.baseline.fit(train_episodes)
-            train_loss = self.inner_loss(train_episodes)
-
-            params = self.policy.update_params(train_loss,
-                step_size=self.fast_lr)
-
+            params = self.adapt(train_episodes)
             valid_loss = self.inner_loss(valid_episodes, params=params)
             losses.append(valid_loss)
         total_rewards = torch.cat([torch.sum(valid_episodes.rewards)
@@ -62,30 +66,9 @@ class MetaLearner(object):
 
         return torch.mean(torch.cat(losses, dim=0)), torch.mean(total_rewards)
 
-    def mean_kl(self, episodes):
-        kls = []
-        for train_episodes, valid_episodes in episodes:
-            self.baseline.fit(train_episodes)
-            train_loss = self.inner_loss(train_episodes)
-
-            params = self.policy.update_params(train_loss,
-                step_size=self.fast_lr)
-
-            pi = self.policy(valid_episodes, params=params)
-            if isinstance(pi, Categorical):
-                pi_old = Categorical(logits=pi.logits.detach())
-            elif isinstance(pi, Normal):
-                pi_old = Normal(loc=pi.loc.detach(), scale=pi.scale.detach())
-            else:
-                raise NotImplementedError('Only `Categorical` and `Normal` '
-                    'policies are valid policies.')
-            # TODO: Use valid_episodes.mask for mean
-            kls.append(kl_divergence(pi, pi_old).mean())
-        return torch.mean(torch.cat(kls, dim=0))
-
     def hessian_vector_product(self, episodes, damping=1e-2):
         def _product(vector):
-            kl = self.mean_kl(episodes)
+            _, kl = self.surrogate_loss(episodes)
             grads = torch.autograd.grad(kl, self.policy.parameters(),
                 create_graph=True)
             flat_grad_kl = torch.cat([grad.view(-1) for grad in grads])
@@ -98,25 +81,39 @@ class MetaLearner(object):
             return flat_grad2_kl + damping * vector
         return _product
 
-    def surrogate_loss(self, episodes, old_pis):
-        losses = []
-        for (train_episodes, valid_episodes), old_pi in zip(episodes, old_pis):
-            self.baseline.fit(train_episodes)
-            train_loss = self.inner_loss(train_episodes)
+    def surrogate_loss(self, episodes, old_pis=None):
+        losses, kls = [], []
+        if old_pis is None:
+            old_pis = [None] * len(episodes)
 
-            params = self.policy.update_params(train_loss,
-                step_size=self.fast_lr)
+        for (train_episodes, valid_episodes), old_pi in zip(episodes, old_pis):
+            params = self.adapt(train_episodes)
             pi = self.policy(valid_episodes.observations, params=params)
+
+            if old_pi is None:
+                if isinstance(pi, Categorical):
+                    old_pi = Categorical(logits=pi.logits.detach())
+                elif isinstance(pi, Normal):
+                    old_pi = Normal(loc=pi.loc.detach(), scale=pi.scale.detach())
+                else:
+                    raise NotImplementedError('Only `Categorical` and `Normal` '
+                        'policies are valid policies.')
 
             values = self.baseline(valid_episodes)
             advantages = valid_episodes.gae(values, tau=1.0)
-
             ratio = torch.exp(pi.log_prob(valid_episodes.actions)
                 - old_pi.log_prob(valid_episodes.actions))
+
             # TODO: Use valid_episodes.mask for mean
             loss = -torch.mean(ratio * advantages)
             losses.append(loss)
-        return torch.mean(torch.cat(losses, dim=0))
+
+            # TODO: Use valid_episodes.mask for mean
+            kl = torch.mean(kl_divergence(pi, old_pi))
+            kls.append(kl)
+
+        return (torch.mean(torch.cat(losses, dim=0)),
+                torch.mean(torch.cat(kls, dim=0)))
 
     def cuda(self, **kwargs):
         self.policy.cuda(**kwargs)
