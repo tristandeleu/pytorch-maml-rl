3fae7c79c364590967fa37cbbc09f44bd3fb8cac
Tristan Deleu <tristan.deleu@gmail.com>
2018-04-12 12:59:45 -0400

    Add value iteration for evaluation of Tabular MDP

---
+++ maml_rl/evaluation.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def value_iteration(transitions, rewards, gamma=0.95, theta=1e-5):
+    rewards = np.expand_dims(rewards, axis=2)
+    values = np.zeros(transitions.shape[0], dtype=np.float32)
+    delta = np.inf
+    while delta >= theta:
+        q_values = np.sum(transitions * (rewards + gamma * values), axis=2)
+        new_values = np.max(q_values, axis=1)
+        delta = np.max(np.abs(new_values - values))
+        values = new_values
+
+    q_values = np.sum(transitions * (rewards + gamma * values), axis=2)
+    pi = np.argmax(q_values, axis=1)
+
+    return values, pi   
