702af9e13f326dac753f6b57cede3d0cddb087b1
Tristan Deleu <tristan.deleu@gmail.com>
2018-04-12 10:09:57 -0400

    Log rewards

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import maml_rl.envs
 import gym
 import numpy as np
-import torch.optim as optim
+import torch
 
 from maml_rl.metalearner import MetaLearner
 from maml_rl.policies import CategoricalMLPPolicy
 from maml_rl.baseline import LinearFeatureBaseline
 from maml_rl.sampler import BatchSampler
 
-from tqdm import trange
+from tqdm import trange, tqdm
 
 sampler = BatchSampler('Bandit-K50-v0', batch_size=100)
 policy = CategoricalMLPPolicy(sampler.envs.observation_space.shape[0],
@@ -16,13 +16,15 @@ policy = CategoricalMLPPolicy(sampler.envs.observation_space.shape[0],
 baseline = LinearFeatureBaseline(sampler.envs.observation_space.shape[0])
 
 metalearner = MetaLearner(sampler, policy, baseline)
-# optimizer = optim.Adam(policy.parameters(), lr=1e-3)
 
 all_rewards = []
-for epoch in trange(1000):
+for epoch in trange(200):
     episodes = metalearner.sample(meta_batch_size=20)
     metalearner.step(episodes)
-    # all_rewards.append(rewards.data[0])
+    rewards = torch.mean(torch.cat([torch.sum(valid_episodes.rewards)
+        for _, valid_episodes in episodes], dim=0))
+    all_rewards.append(rewards.data[0])
+    tqdm.write('{0}'.format(rewards.data[0]))
 
 with open('tmp/rewards.npy', 'wb') as f:
     np.save(f, np.asarray(all_rewards))
