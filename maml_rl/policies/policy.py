d6828e8721e46ea64e09f368db534cdfb56fb3e2
Tristan Deleu <tristan.deleu@gmail.com>
2018-06-14 11:27:01 -0400

    Remove state_dict from Policy

--- maml_rl/policies/policy.py
+++ maml_rl/policies/policy.py
@@ -16,16 +16,3 @@ class Policy(nn.Module):
             updated_params[name] = param - step_size * grad
 
         return updated_params
-
-    def state_dict(self, params=None, destination=None, prefix='', keep_vars=False):
-        if params is None:
-            destination = super(Policy, self).state_dict(
-                destination=destination, prefix=prefix, keep_vars=keep_vars)
-        else:
-            if destination is None:
-                destination = OrderedDict()
-            for name, param in params.items():
-                if param is not None:
-                    destination[prefix + name] = param if keep_vars else param.data
-
-        return destination
