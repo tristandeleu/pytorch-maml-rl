72056013f656b49ae83342495dfd88391fd8e5d8
Tristan Deleu <tristan.deleu@gmail.com>
2018-06-14 17:16:35 -0400

    Update HalfCheetahVelEnv

--- maml_rl/envs/mujoco/half_cheetah_vel.py
+++ maml_rl/envs/mujoco/half_cheetah_vel.py
@@ -4,61 +4,43 @@ from gym.envs.mujoco import mujoco_env
 from gym import spaces
 
 class HalfCheetahVelEnv(mujoco_env.MujocoEnv, utils.EzPickle):
-    def __init__(self, goal=0):
-        self._task = 0
-        self._goal_vel = goal
-        #temporary action_space
-        self.action_space = spaces.Box(low=0.5, high=1, shape=(1,), dtype=np.float32)
-
+    def __init__(self, **task):
+        self._task = task
+        self._goal_vel = task.get('velocity', 0.0)
         mujoco_env.MujocoEnv.__init__(self, 'half_cheetah.xml', 5)
         utils.EzPickle.__init__(self)
 
     def step(self, action):
-        xposbefore = self.get_body_com("torso")[0]
+        xposbefore = self.get_body_com("torso").flat[0]
         self.do_simulation(action, self.frame_skip)
-        xposafter = self.get_body_com("torso")[0]
+        xposafter = self.get_body_com("torso").flat[0]
 
         forward_vel = (xposafter - xposbefore) / self.dt
-        forward_reward = -1.*abs(forward_vel - self._goal_vel)
-
-        lb = self.action_space.low
-        ub = self.action_space.high
-        action = np.clip(action, lb, ub)
+        forward_reward = -1.0 * abs(forward_vel - self._goal_vel)
         ctrl_cost = 0.5 * 1e-1 * np.sum(np.square(action))
 
+        observation = self._get_obs()
         reward = forward_reward - ctrl_cost
         done = False
-        observation = self._get_obs()
-        return (observation, reward, done, dict(
-            reward_forward=forward_reward,
-            reward_ctrl=-ctrl_cost,
-            task=self._task))
+        infos = dict(reward_forward=forward_reward,
+            reward_ctrl=-ctrl_cost, task=self._task)
+        return (observation, reward, done, infos)
 
     def _get_obs(self):
         return np.concatenate([
             self.sim.data.qpos.flat[1:],
             self.sim.data.qvel.flat,
-            self.get_body_com("torso"),
-        ]).astype(np.float32).reshape(-1)
+            self.get_body_com("torso").flat,
+        ]).astype(np.float32).flatten()
 
     def sample_tasks(self, num_tasks):
-        velocities = np.random.uniform(0.0, 2.0, size=(num_tasks,))
+        velocities = self.np_random.uniform(0.0, 2.0, size=(num_tasks,))
         tasks = [{'velocity': velocity} for velocity in velocities]
         return tasks
 
     def reset_task(self, task):
         self._task = task
         self._goal_vel = task['velocity']
-
-    def reset(self):
-
-        qpos = self.init_qpos + np.random.normal(size=self.init_qpos.shape) * 0.01
-        qvel = self.init_qvel  + np.random.normal(size=self.init_qvel.shape) * 0.1
-
-        #qpos = self.init_qpos + self.np_random.uniform(size=self.model.nq, low=-.1, high=.1)
-        #qvel = self.init_qvel + self.np_random.randn(self.model.nv) * 0.1
-        self.set_state(qpos, qvel)
-        return self._get_obs()
 
     def viewer_setup(self):
         self.viewer.cam.type = 1
