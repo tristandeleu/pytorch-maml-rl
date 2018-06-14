551436a8106f3292db2f7d1a6da3462f2aa8bb6b
Tristan Deleu <tristan.deleu@gmail.com>
2018-06-14 11:21:10 -0400

    Make NormalMLPPolicy closer to original

--- maml_rl/policies/normal_mlp.py
+++ maml_rl/policies/normal_mlp.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,13 +7,19 @@ from torch.distributions import Normal
 from collections import OrderedDict
 from maml_rl.policies.policy import Policy
 
+def weight_init(module):
+    if isinstance(module, nn.Linear):
+        nn.init.xavier_uniform_(module.weight)
+        module.bias.data.zero_()
+
 class NormalMLPPolicy(Policy):
-    def __init__(self, input_size, output_size,
-                 hidden_sizes=(), nonlinearity=F.relu):
+    def __init__(self, input_size, output_size, hidden_sizes=(),
+                 nonlinearity=F.relu, init_std=1.0, min_std=1e-6):
         super(NormalMLPPolicy, self).__init__(
             input_size=input_size, output_size=output_size)
         self.hidden_sizes = hidden_sizes
         self.nonlinearity = nonlinearity
+        self.min_log_std = math.log(min_std)
         self.num_layers = len(hidden_sizes) + 1
 
         layer_sizes = (input_size,) + hidden_sizes
@@ -20,7 +27,10 @@ class NormalMLPPolicy(Policy):
             self.add_module('layer{0}'.format(i),
                 nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
         self.add_module('mu', nn.Linear(layer_sizes[-1], output_size))
-        self.add_module('sigma', nn.Linear(layer_sizes[-1], output_size))
+
+        self.sigma = nn.Parameter(torch.Tensor(output_size))
+        self.sigma.data.fill_(math.log(init_std))
+        self.apply(weight_init)
 
     def forward(self, input, params=None):
         if params is None:
@@ -33,7 +43,6 @@ class NormalMLPPolicy(Policy):
             output = self.nonlinearity(output)
         mu = F.linear(output, weight=params['mu.weight'],
             bias=params['mu.bias'])
-        sigma = F.linear(output, weight=params['sigma.weight'],
-            bias=params['sigma.bias'])
+        scale = torch.exp(torch.clamp(params['sigma'], min=self.min_log_std))
 
-        return Normal(loc=mu, scale=F.softplus(sigma))
+        return Normal(loc=mu, scale=scale)
