Fix NormalizedRewardWrapper running variance

maml_rl/envs/normalized_env.py
@@ -75,5 +75,5 @@ class NormalizedRewardWrapper(gym.RewardWrapper):
 
     def reward(self, reward):
         self._mean = (1.0 - self.alpha) * self._mean + self.alpha * reward
-        self._var = (1.0 - self.alpha) * self._var + self.alpha * np.square(reward, self._mean)
+        self._var = (1.0 - self.alpha) * self._var + self.alpha * np.square(reward - self._mean)
         return (reward - self._mean) / (np.sqrt(self._var) + self.epsilon)