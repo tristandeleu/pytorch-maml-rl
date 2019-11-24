01b8f6eef41c36f12019adfc23ace2a100d77610
Tristan Deleu <tristan.deleu@gmail.com>
2019-11-24 12:34:49 -0500

    Update SyncVectorEnv to use VectorEnv from gym

--- maml_rl/envs/sync_vector_env.py
+++ maml_rl/envs/sync_vector_env.py
@@ -1,68 +1,59 @@
 import numpy as np
-import gym
+from copy import deepcopy
+
+from gym.vector import SyncVectorEnv as SyncVectorEnv_
+from gym.vector.utils import concatenate, create_empty_array
 
 
-class SyncVectorEnv(gym.Env):
-    def __init__(self, env_fns, observation_space=None, action_space=None):
-        super(SyncVectorEnv, self).__init__()
-        self.env_fns = env_fns
-        self.num_envs = len(env_fns)
-        self.envs = [env_fn() for env_fn in env_fns]
-
-        if (observation_space is None) or (action_space is None):
-            observation_space = observation_space or self.envs[0].observation_space
-            action_space = action_space or self.envs[0].action_space
-        self.observation_space = observation_space
-        self.action_space = action_space
-
-        self._dones = np.zeros((self.num_envs,), dtype=np.bool_)
-
-    def seed(self, seeds=None):
-        if seeds is None:
-            seeds = [None for _ in range(self.num_envs)]
-        if isinstance(seeds, int):
-            seeds = [seeds + i for i in range(self.num_envs)]
-        assert len(seeds) == self.num_envs
-
-        for env, seed in zip(self.envs, seeds):
-            env.seed(seed)
+class SyncVectorEnv(SyncVectorEnv_):
+    def __init__(self,
+                 env_fns,
+                 observation_space=None,
+                 action_space=None,
+                 **kwargs):
+        super(SyncVectorEnv, self).__init__(env_fns,
+                                            observation_space=observation_space,
+                                            action_space=action_space,
+                                            **kwargs)
+        for env in self.envs:
+            if not hasattr(env.unwrapped, 'reset_task'):
+                raise ValueError('The environment provided is not a '
+                                 'meta-learning environment. It does not have '
+                                 'the method `reset_task` implemented.')
 
     def reset_task(self, task):
         for env in self.envs:
             env.unwrapped.reset_task(task)
 
-    def reset(self):
-        self._dones[:] = False
-        observations = []
-        for env in self.envs:
-            observation = env.reset()
-            observations.append(observation)
-
-        return np.stack(observations, axis=0)
-
-    def step(self, actions):
-        observations, infos = [], []
+    def step_wait(self):
+        observations_list, infos = [], []
         batch_ids, j = [], 0
-        rewards = np.zeros((len(actions),), dtype=np.float_)
+        num_actions = len(self._actions)
+        rewards = np.zeros((num_actions,), dtype=np.float_)
+        dones = deepcopy(self._dones)
         for i, env in enumerate(self.envs):
             if self._dones[i]:
                 continue
 
-            observation, rewards[j], self._dones[i], info = env.step(actions[j])
+            action = self._actions[j]
+            observation, rewards[j], self._dones[i], info = env.step(action)
             batch_ids.append(i)
 
             if not self._dones[i]:
-                observations.append(observation)
+                observations_list.append(observation)
                 infos.append(info)
             j += 1
-        assert len(actions) == j
+        assert num_actions == j
 
-        if observations:
-            observations = np.stack(observations, axis=0)
+        if observations_list:
+            observations = create_empty_array(self.single_observation_space,
+                                              n=len(observations_list),
+                                              fn=np.zeros)
+            concatenate(observations_list,
+                        observations,
+                        self.single_observation_space)
+        else:
+            observations = None
 
         return (observations, rewards, np.copy(self._dones),
                 {'batch_ids': batch_ids, 'infos': infos})
-
-    def close(self):
-        for env in self.envs:
-            env.close()
