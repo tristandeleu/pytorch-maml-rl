c30fa04bbd5e73c229c110828f5154df9517e595
Tristan Deleu <tristan.deleu@gmail.com>
2020-01-07 15:48:48 +0100

    Format policies

--- maml_rl/policies/categorical_mlp.py
+++ maml_rl/policies/categorical_mlp.py
@@ -12,10 +12,13 @@ class CategoricalMLPPolicy(Policy):
     with discrete action spaces (eg. `TabularMDPEnv`). The code is adapted from 
     https://github.com/cbfinn/maml_rl/blob/9c8e2ebd741cb0c7b8bf2d040c4caeeb8e06cc95/sandbox/rocky/tf/policies/maml_minimal_categorical_mlp_policy.py
     """
-    def __init__(self, input_size, output_size,
-                 hidden_sizes=(), nonlinearity=F.relu):
-        super(CategoricalMLPPolicy, self).__init__(
-            input_size=input_size, output_size=output_size)
+    def __init__(self,
+                 input_size,
+                 output_size,
+                 hidden_sizes=(),
+                 nonlinearity=F.relu):
+        super(CategoricalMLPPolicy, self).__init__(input_size=input_size,
+                                                   output_size=output_size)
         self.hidden_sizes = hidden_sizes
         self.nonlinearity = nonlinearity
         self.num_layers = len(hidden_sizes) + 1
@@ -23,20 +26,23 @@ class CategoricalMLPPolicy(Policy):
         layer_sizes = (input_size,) + hidden_sizes + (output_size,)
         for i in range(1, self.num_layers + 1):
             self.add_module('layer{0}'.format(i),
-                nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
+                            nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
+
         self.apply(weight_init)
 
     def forward(self, input, params=None):
         if params is None:
             params = OrderedDict(self.named_parameters())
+
         output = input
         for i in range(1, self.num_layers):
             output = F.linear(output,
-                weight=params['layer{0}.weight'.format(i)],
-                bias=params['layer{0}.bias'.format(i)])
+                              weight=params['layer{0}.weight'.format(i)],
+                              bias=params['layer{0}.bias'.format(i)])
             output = self.nonlinearity(output)
+
         logits = F.linear(output,
-            weight=params['layer{0}.weight'.format(self.num_layers)],
-            bias=params['layer{0}.bias'.format(self.num_layers)])
+                          weight=params['layer{0}.weight'.format(self.num_layers)],
+                          bias=params['layer{0}.bias'.format(self.num_layers)])
 
         return Categorical(logits=logits)
--- maml_rl/policies/normal_mlp.py
+++ maml_rl/policies/normal_mlp.py
@@ -14,10 +14,15 @@ class NormalMLPPolicy(Policy):
     `HalfCheetahDir`). The code is adapted from 
     https://github.com/cbfinn/maml_rl/blob/9c8e2ebd741cb0c7b8bf2d040c4caeeb8e06cc95/sandbox/rocky/tf/policies/maml_minimal_gauss_mlp_policy.py
     """
-    def __init__(self, input_size, output_size, hidden_sizes=(),
-                 nonlinearity=F.relu, init_std=1.0, min_std=1e-6):
-        super(NormalMLPPolicy, self).__init__(
-            input_size=input_size, output_size=output_size)
+    def __init__(self,
+                 input_size,
+                 output_size,
+                 hidden_sizes=(),
+                 nonlinearity=F.relu,
+                 init_std=1.0,
+                 min_std=1e-6):
+        super(NormalMLPPolicy, self).__init__(input_size=input_size,
+                                              output_size=output_size)
         self.hidden_sizes = hidden_sizes
         self.nonlinearity = nonlinearity
         self.min_log_std = math.log(min_std)
@@ -26,24 +31,28 @@ class NormalMLPPolicy(Policy):
         layer_sizes = (input_size,) + hidden_sizes
         for i in range(1, self.num_layers):
             self.add_module('layer{0}'.format(i),
-                nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
-        self.mu = nn.Linear(layer_sizes[-1], output_size)
+                            nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
 
+        self.mu = nn.Linear(layer_sizes[-1], output_size)
         self.sigma = nn.Parameter(torch.Tensor(output_size))
         self.sigma.data.fill_(math.log(init_std))
+
         self.apply(weight_init)
 
     def forward(self, input, params=None):
         if params is None:
             params = OrderedDict(self.named_parameters())
+
         output = input
         for i in range(1, self.num_layers):
             output = F.linear(output,
-                weight=params['layer{0}.weight'.format(i)],
-                bias=params['layer{0}.bias'.format(i)])
+                              weight=params['layer{0}.weight'.format(i)],
+                              bias=params['layer{0}.bias'.format(i)])
             output = self.nonlinearity(output)
-        mu = F.linear(output, weight=params['mu.weight'],
-            bias=params['mu.bias'])
+
+        mu = F.linear(output,
+                      weight=params['mu.weight'],
+                      bias=params['mu.bias'])
         scale = torch.exp(torch.clamp(params['sigma'], min=self.min_log_std))
 
         return Normal(loc=mu, scale=scale)
