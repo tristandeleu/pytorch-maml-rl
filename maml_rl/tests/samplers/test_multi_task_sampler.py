b358986281064fa585cbba96b22bc5b44933a18a
Tristan Deleu <tristan.deleu@gmail.com>
2020-01-11 14:41:58 +0100

    Fix the tests for the sampler

--- maml_rl/tests/samplers/test_multi_task_sampler.py
+++ maml_rl/tests/samplers/test_multi_task_sampler.py
@@ -22,6 +22,7 @@ def test_init(env_name, num_workers):
     baseline = LinearFeatureBaseline(get_input_size(env))
 
     sampler = MultiTaskSampler(env_name,
+                               {}, # env_kwargs
                                batch_size,
                                policy,
                                baseline,
@@ -32,8 +33,9 @@ def test_init(env_name, num_workers):
 @pytest.mark.parametrize('env_name', ['TabularMDP-v0', '2DNavigation-v0'])
 @pytest.mark.parametrize('batch_size', [1, 7])
 @pytest.mark.parametrize('num_tasks', [1, 5])
+@pytest.mark.parametrize('num_steps', [1, 3])
 @pytest.mark.parametrize('num_workers', [1, 3])
-def test_sample(env_name, batch_size, num_tasks, num_workers):
+def test_sample(env_name, batch_size, num_tasks, num_steps, num_workers):
     # Environment
     env = gym.make(env_name)
     env.close()
@@ -42,16 +44,19 @@ def test_sample(env_name, batch_size, num_tasks, num_workers):
     baseline = LinearFeatureBaseline(get_input_size(env))
 
     sampler = MultiTaskSampler(env_name,
+                               {}, # env_kwargs
                                batch_size,
                                policy,
                                baseline,
                                num_workers=num_workers)
     tasks = sampler.sample_tasks(num_tasks=num_tasks)
-    train_episodes, valid_episodes = sampler.sample(tasks)
+    train_episodes, valid_episodes = sampler.sample(tasks,
+                                                    num_steps=num_steps)
     sampler.close()
 
-    assert len(train_episodes) == num_tasks
-    assert isinstance(train_episodes[0], BatchEpisodes)
+    assert len(train_episodes) == num_steps
+    assert len(train_episodes[0]) == num_tasks
+    assert isinstance(train_episodes[0][0], BatchEpisodes)
 
     assert len(valid_episodes) == num_tasks
     assert isinstance(valid_episodes[0], BatchEpisodes)
