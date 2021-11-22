70f0d58ab4ac4a9b570c2a14f19185abe405f159
rafael <rafael.sterzinger@gmx.at>
2021-11-22 12:41:31 +0100

    Fix bandit.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ def main(args):
         torch.manual_seed(args.seed)
         torch.cuda.manual_seed_all(args.seed)
 
-    env = gym.make(config['env-name'], **config['env-kwargs'])
+    env = gym.make(config['env-name'], **config.get('env-kwargs',{}))
     env.close()
 
     # Policy
@@ -36,7 +36,7 @@ def main(args):
 
     # Sampler
     sampler = MultiTaskSampler(config['env-name'],
-                               env_kwargs=config['env-kwargs'],
+                               env_kwargs=config.get('env-kwargs', {}),
                                batch_size=config['fast-batch-size'],
                                policy=policy,
                                baseline=baseline,
